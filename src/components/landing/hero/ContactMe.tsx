4778c72b01487685b1308c78341c317c1fb0eb4b
nevercomex <luisjavier2012@gmail.com>
2024-08-28 00:38:58 -0400

    chore: Update landing page footer link and text styles

--- src/components/landing/hero/ContactMe.tsx
+++ src/components/landing/hero/ContactMe.tsx
@@ -1,11 +1,11 @@
 const ContactMe = () => (
   <section className="text-center pt-12">
     <div className="footer-copyright ">
-      <div className=" text-gray-800 text-5xl ">
+      <div className=" text-gray-800 text-3xl ">
         <a href="https://wa.me/18094805488">+1 (809) 480-5488</a>
       </div>
       <br />
-      <div className="text-gray-700 text-4xl ">
+      <div className="text-gray-700 text-2xl ">
         <a href="mailto:datsercom@claro.net.do ">datsercom@claro.net.do</a>
       </div>
     </div>
